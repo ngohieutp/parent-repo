8ac4fea925efa418d6d135508873875654afbd02
hieund7 <ngohieutp@gmail.com>
2022-10-02 17:18:25 +0700

    Add sub-repo

--- src/main/java/com/hhnt/Main.java
+++ src/main/java/com/hhnt/Main.java
@@ -1,8 +1,11 @@
 package com.hhnt;
 
+import hhnt.TextUtil;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(TextUtil.trim("Hi you    "));
     }
 
 }
